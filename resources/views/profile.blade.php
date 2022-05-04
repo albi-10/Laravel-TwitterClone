16d0e32bc0218ab56746a07187faf90873469373
albi <albijan.berisha@stud.uni-due.de>
2022-05-04 12:19:27 +0200

    ERM Diagram

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -20,6 +20,7 @@
         </div>
     </div>
 <script>
+    // Bild für die vorschau zum Profiländern
     var loadFile = function(event) {
         var output = document.getElementById('output');
         output.src = URL.createObjectURL(event.target.files[0]);
